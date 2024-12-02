b6ccfd321fdb707eb2453436a26699edafb16df8
Viktor Lazar <vlazar@outlook.com>
2024-12-02 09:59:26 +0100

    added rpi kiosk script

---
+++ run-kiosk.sh
@@ -0,0 +1,2 @@
+sleep 4
+/bin/chromium-browser --kiosk --ozone-platform=wayland --start-maximized --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar file:///home/mzp-1/mzp-kiosk/kiosk-1/index.html
