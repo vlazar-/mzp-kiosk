66ea440227df7ce15350a4a1319f09cbc4fb25c7
Viktor Lazar <vlazar@outlook.com>
2024-12-02 10:09:14 +0100

    init script

--- init.sh
+++ init.sh
@@ -2,5 +2,4 @@ cd /home/mzp-1
 git clone https://github.com/vlazar-/mzp-kiosk.git
 cd mzp-kiosk
 sudo chmod +x run-kiosk.sh
-mv wayfire.ini ../.config/wayfire.ini
-reboot
+mv wayfire.ini /home/mzp-1/.config/wayfire.ini
