ec3a3ecd23b68e1fb44f045ac2c304efd39cd60a
Viktor Lazar <vlazar@outlook.com>
2024-12-02 18:57:09 +0100

    updated init scripts for kiosk

--- run-kiosk.sh
+++ run-kiosk.sh
@@ -1,2 +1,2 @@
 sleep 4
-/bin/chromium-browser --kiosk --ozone-platform=wayland --start-maximized --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar file:///home/mzp-1/mzp-kiosk/kiosk-1/index.html
+/bin/chromium-browser --kiosk --ozone-platform=wayland --start-maximized --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar --no-first-run file:///home/mzp-1/mzp-kiosk/kiosk-1/index.html
