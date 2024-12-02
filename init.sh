c636199f9071146eab913d655930e9119f7a3fc3
Viktor Lazar <vlazar@outlook.com>
2024-12-02 19:43:37 +0100

    updated init script

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ sudo apt install ydotool
 sudo chmod +x run-kiosk.sh
 sudo chmod +x hide-cursor.sh
 
-mv wayfire.ini /home/mzp-1/.config/wayfire.ini
+unalias cp
+cp -f wayfire.ini /home/mzp-1/.config/wayfire.ini
