227198598a8a9ed165fd33968af7dd4dc7ccc449
Viktor Lazar <vlazar@outlook.com>
2024-12-02 20:21:49 +0100

    updated kiosk scripts

--- init.sh
+++ init.sh
@@ -1,7 +1,14 @@
 sudo apt install ydotool
 
+pcmanfm --set-wallpaper="img/splash.png"
+
 sudo chmod +x run-kiosk.sh
 sudo chmod +x hide-cursor.sh
 
 unalias cp
-cp -f wayfire.ini /home/mzp-1/.config/wayfire.ini
+
+sudo mv /usr/share/plymouth/themes/pix/splash.png /usr/share/plymouth/themes/pix/splash-default.png
+sudo cp -f img/splash.png /usr/share/plymouth/themes/pix
+
+sudo mv /home/mzp-1/.config/wayfire.ini /home/mzp-1/.config/wayfire-default.ini
+sudo cp -f wayfire.ini /home/mzp-1/.config/wayfire.ini
